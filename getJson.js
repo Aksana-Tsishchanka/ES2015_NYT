6a21a2ea263ea5491be730028af3a3f207a85606
Aksana <aksana@Aksanas-MacBook-Pro.local>
2016-06-28 22:43:00 +0300

    Design completed

--- getJson.js
+++ getJson.js
@@ -3,6 +3,7 @@ const init = {
     url: url,
     method: 'GET',
 };
+const imageType = 'mediumThreeByTwo210';
 
 
 fetch(url, init)
@@ -10,37 +11,39 @@ fetch(url, init)
         return response.json();
     })
     .then(function(json){
-        paintSections(json);
+        addSections(json);
     });
 
 
-function paintSections(json) {
-    var mainDiv =  document.querySelector('div.main')
-
-    var divContainer = document.createElement('div');
-    divContainer.className = 'flexbox-container';
-
+function addSections(json) {
     let { section, results : arrSections, copyright } = json;
 
+    let divContainer = document.createElement('div');
+    divContainer.className = 'flexbox-container';
+
     let header = document.querySelector('header');
-    let headerSection = document.createElement('h3');
-    headerSection.textContent = section;
-    header.appendChild(headerSection);
+    header.innerHTML += `<h3>${section}</h3>`;
 
     for (var objSection of arrSections) {
         divContainer.appendChild(createSection(objSection));
     }
+
+    let mainDiv =  document.querySelector('div.main')
     mainDiv.appendChild(divContainer);
+
     document.body.appendChild(createFooter(copyright));
 }
+
+
 
 function createSection(objSection) {
     let { short_url, title, abstract, multimedia, byline, published_date } = objSection;
 
     let sectionEl = document.createElement('div');
     sectionEl.className = 'introSection';
+    sectionEl.setAttribute('onclick',`window.location='${short_url}';`);
 
-    let titleLink = `<h2><a href=${short_url}>${title}</a></h2>`;
+    let titleLink = `<h2>${title}</a></h2>`;
     let paragraf = `<p>${abstract}</p>`;
     let time = moment(published_date).fromNow();
     let signInfo =`<span class='time'>${time}</span><span class='author'> ${byline}</span>`;
@@ -50,7 +53,7 @@ function createSection(objSection) {
     textContainer.innerHTML = titleLink + paragraf + signInfo;
 
     if ( multimedia.length > 0) {
-        let { url, height, width, type, caption } = getMedia(multimedia, 'mediumThreeByTwo210');
+        let { url, height, width, type, caption } = getMedia(multimedia, imageType);
         if (type === 'image') {
          sectionEl.innerHTML = createImage(url, height, width, caption);
         }
@@ -76,9 +79,3 @@ function createFooter(footerText) {
     footer.textContent = footerText.replace('Copyright (c)','©');
     return footer;
 }
-
-//1) Data formatting
-//2) RegExp
-//3) https://kadira.io/blog/other/top-es2015-features-in-15-minutes SPREADING for images
-// https://github.com/lukehoban/es6features#generators
-//http://es6-features.org/#DateTimeFormatting  check features
